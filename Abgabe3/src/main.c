279574509c9179cf791243973f2ce87f2e7f0a93
Oszimilian <student@ubuntu>
2022-11-22 07:27:41 -0800

    Oszimilian: Base Project

---
+++ Abgabe3/src/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: 22.11.2022
+ *      Author: student
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("Abgabe3 \n");
+
+
+	int wait;
+	scanf("%d", &wait);
+	return EXIT_SUCCESS;
+}
