85a3ed153ce42ce7d59e47b0e5876aa9a8957d7c
student <student@ubuntu>
2022-12-02 13:12:02 +0100

    repaierd

--- AdventOfCode2022/src/Tag01/Tag01.c
+++ AdventOfCode2022/src/Tag01/Tag01.c
@@ -16,24 +16,22 @@ void doDay01TaskA() {
 
 	printf("Day 01");
 
-	// Textdatei einlesen
+
 	size_t n = 1;
 	char buf[BUFSZ] = "";
 	FILE* fp = fopen("src/Tag01/inputA.txt","r");
 
-	while (fgets (buf, BUFSZ, fp)) {        /* read each line in file */
-		size_t len = strlen (buf);          /* get buf length */
-		if (len && buf[len-1] == '\n')      /* check last char is '\n' */
-			buf[--len] = 0;                 /* overwrite with nul-character */
-		else {   /* line too long or non-POSIX file end, handle as required */
-			// Zeile enthaelt Zahl
-			printf ("line[%2zu] : %s\n", n, buf);
-			continue;
-		}   /* output line (or "empty" if line was empty) */
-		// Zeile ist leer
-		printf ("line[%2zu] : %s\n", n++, len ? buf : "empty");
-	}
-	if (fp != stdin) fclose (fp);           /* close file if not stdin */
+	    while (fgets (buf, BUFSZ, fp)) {        /* read each line in file */
+	        size_t len = strlen (buf);          /* get buf length */
+	        if (len && buf[len-1] == '\n')      /* check last char is '\n' */
+	            buf[--len] = 0;                 /* overwrite with nul-character */
+	        else {   /* line too long or non-POSIX file end, handle as required */
+	            printf ("line[%2zu] : %s\n", n, buf);
+	            continue;
+	        }   /* output line (or "empty" if line was empty) */
+	        printf ("line[%2zu] : %s\n", n++, len ? buf : "empty");
+	    }
+	    if (fp != stdin) fclose (fp);           /* close file if not stdin */
 }
 
 void doDay01TaskB() {
